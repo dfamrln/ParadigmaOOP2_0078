e0c2f0efe08545fb4bedf153673a31fb15362ecd
dfamrln <mhdffa1@gmail.com>
2025-06-03 15:46:22 +0700

    Update noPewarisan.cpp

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -14,3 +14,16 @@ public:
     }
 };
 
+class derivedClass : public baseClass2 {
+public:
+    void perkenalan1() {
+        cout << "Halo saya Function dari derived Class";
+    }
+};
+
+int main() {
+    derivedClass a;
+    a.perkenalan1();
+
+    return 0;
+}
